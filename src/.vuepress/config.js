102f1cc00f974b9ea2210e8ee0af28966dd2d26e
kevsteramp <kevinamparado13@gmail.com>
2023-11-20 09:42:16 +0800

    added markdown on config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -56,7 +56,9 @@ module.exports = {
 					},
 					{
 						title: "Junior Cloud Architects",
-						children: [],
+						children: [
+							"dcc/members/bryce-madelo
+						],
 					},
 					{
 						title: "Cloud Guru",
