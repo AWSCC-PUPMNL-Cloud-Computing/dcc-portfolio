2084ee664db735057cbd0f79a8e53bc8262c6a06
gailespns <gailecespinosa@iskolarngbayan.pup.edu.ph>
2023-11-23 19:40:39 +0800

    add portfolio: Gaile:

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -56,7 +56,9 @@ module.exports = {
 					},
 					{
 						title: "Junior Cloud Architects",
-						children: [],
+						children: [
+							"/dcc/members/gaile-espinosa",
+						],
 					},
 					{
 						title: "Cloud Guru",
