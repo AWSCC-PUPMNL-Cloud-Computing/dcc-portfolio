1ff5d40b123bd2f4d7695e60ebb775dcd912a1d7
egglou <miguel.rafaellouie2003@gmail.com>
2023-12-19 09:54:39 +0800

    Update VuePress configuration and add new member profile

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,107 +1,111 @@
 const { description } = require("../../package");
 
 module.exports = {
-	base: "/",
-	title: "AWSCC - PUP Manila: Cloud Computing",
-	description: description,
+    base: "/",
+    title: "AWSCC - PUP Manila: Cloud Computing",
+    description: description,
 
-	head: [
-		["meta", { name: "theme-color", content: "#9C5CAA" }],
-		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-		["meta",{ name: "apple-mobile-web-app-status-bar-style", content: "black" },],
-		["link",{rel: "icon",type: "image/png",href: "https://raw.githubusercontent.com/AWSCC-PUPMNL-Cloud-Computing/assets/main/Portfolio-Site/DCC-LOGO-Portfolio.png",},],
-	],
-	themeConfig: {
-		logo: "https://raw.githubusercontent.com/AWSCC-PUPMNL-Cloud-Computing/assets/main/Portfolio-Site/DCC-LOGO-Portfolio.png",
-		repo: "",
-		nav: [
-			{
-				text: "DCC",
-				link: "/dcc/",
-			},
-			{
-				text: "GitHub",
-				link: "https://github.com/AWSCC-PUPMNL-Cloud-Computing",
-			},
-		],
-		sidebar: [
-			{
-				title: "Projects",
-				collapsable: false,
-				children: [
-					{
-						title: "Department Projects",
-						children: [
-							"/dcc/projects/csv-json",
-						],
-					},
-					{
-						title: "Personal Projects",
-						children: [],
-					}
-				]
-			},
-			{	
-				title: "Members",
-				collapsable: false,
-				base: '/dcc/members/',
-				children: [
-					{
-						title: "Head and Co-Head",
-						children: [
-							"/dcc/members/rafael-miguel",
-						],
-					},
-					{
-						title: "Senior Cloud Architects",
-						children: [
-							"/dcc/members/kevin-amparado",
-							"/dcc/members/jiro-agad",
-              				"dcc/members/anthony-hinay", 
-			  				"dcc/members/jericho-delrosario",
-						],
-					},
-					{
-						title: "Junior Cloud Architects",
-						children: [
-							"/dcc/members/joyrel-baladjay",
-							"/dcc/members/gaile-espinosa",
-							"/dcc/members/daniella-simara",
-							"/dcc/members/cyrus-jimenez",
-							"/dcc/members/dion-sobrevilla",
-							"/dcc/members/glenn-gabrillo",
-							"/dcc/members/neilxander-sabulao",
-							"/dcc/members/bea-romero",
-							"/dcc/members/francine-jara",
-							"/dcc/members/paul-morada",
-							"/dcc/members/bryce-madelo",
-							"/dcc/members/alvin-feliciano",
-							"/dcc/members/jeiren-mondejar",
-							"/dcc/members/kaysea-bognot",
-							"/dcc/members/cydrick-cortel",
-							"/dcc/members/rhome-saringayat",
-							"/dcc/members/jerald-adlawon",
-							"/dcc/members/jericho-marquez",
-							"/dcc/members/aidan-tiu",
-							"/dcc/members/rainier-reyes",
-							"/dcc/members/tessa-deguzman",
-							"/dcc/members/michael-velasquez",
-						],
-					},
-					{
-						title: "Cloud Guru",
-						children: [
-							"/dcc/members/alf",
-						]
-					},
-				]
-			},
-		],
-	},
+    head: [
+        ["meta", { name: "theme-color", content: "#9C5CAA" }],
+        ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+        [
+            "meta",
+            { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+        ],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                href: "https://raw.githubusercontent.com/AWSCC-PUPMNL-Cloud-Computing/assets/main/Portfolio-Site/DCC-LOGO-Portfolio.png",
+            },
+        ],
+    ],
+    themeConfig: {
+        logo: "https://raw.githubusercontent.com/AWSCC-PUPMNL-Cloud-Computing/assets/main/Portfolio-Site/DCC-LOGO-Portfolio.png",
+        repo: "",
+        nav: [
+            {
+                text: "DCC",
+                link: "/dcc/",
+            },
+            {
+                text: "GitHub",
+                link: "https://github.com/AWSCC-PUPMNL-Cloud-Computing",
+            },
+        ],
+        sidebar: [
+            {
+                title: "Projects",
+                collapsable: false,
+                children: [
+                    {
+                        title: "Department Projects",
+                        children: ["/dcc/projects/csv-json"],
+                    },
+                    {
+                        title: "Personal Projects",
+                        children: [],
+                    },
+                ],
+            },
+            {
+                title: "Members",
+                collapsable: false,
+                base: "/dcc/members/",
+                children: [
+                    {
+                        title: "Head and Co-Head",
+                        children: ["/dcc/members/rafael-miguel"],
+                    },
+                    {
+                        title: "Senior Cloud Architects",
+                        children: [
+                            "dcc/members/anthony-hinay",
+                            "dcc/members/jericho-delrosario",
+                            "/dcc/members/jiro-agad",
+                            "/dcc/members/kevin-amparado",
+                        ],
+                    },
+                    {
+                        title: "Junior Cloud Architects",
+                        children: [
+                            "/dcc/members/aidan-tiu",
+                            "/dcc/members/alvin-feliciano",
+                            "/dcc/members/bea-romero",
+                            "/dcc/members/bryce-madelo",
+                            "/dcc/members/cydrick-cortel",
+                            "/dcc/members/cyrus-jimenez",
+                            "/dcc/members/daniella-simara",
+                            "/dcc/members/dion-sobrevilla",
+                            "/dcc/members/francine-jara",
+                            "/dcc/members/gaile-espinosa",
+                            "/dcc/members/glenn-gabrillo",
+                            "/dcc/members/jeiren-mondejar",
+                            "/dcc/members/jerald-adlawon",
+                            "/dcc/members/jericho-marquez",
+                            "/dcc/members/joyrel-baladjay",
+                            "/dcc/members/kaysea-bognot",
+                            "/dcc/members/michael-velasquez",
+                            "/dcc/members/neilxander-sabulao",
+                            "/dcc/members/paul-morada",
+                            "/dcc/members/rainier-reyes",
+                            "/dcc/members/rhome-saringayat",
+                            "/dcc/members/tessa-deguzman",
+                        ],
+                    },
+                    {
+                        title: "Cloud Guru",
+                        children: ["/dcc/members/1alf"],
+                    },
+                ],
+            },
+        ],
+    },
 
-	/**
-	 * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-	 */
-	plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+    /**
+     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+     */
+    plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
 };
 
